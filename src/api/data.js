4db8ca4868995c597f8e7c3b46f6f3d240c5f20f
abir <abirchandrabhushan@gmail.com>
2020-04-14 01:39:40 +0530

    add (helper) API method to filter country by slug

--- src/api/data.js
+++ src/api/data.js
@@ -1,5 +1,4 @@
 const SERVER = 'https://api.covid19api.com/'
-const SERVER_2 = 'https://coronavirus-tracker-api.herokuapp.com/'
 
 /**
  * Pulls Global Summary (Confirmed, Deaths, Recovered) of the pandemic from the Postman API
@@ -15,28 +14,6 @@ function requestSummary() {
       .catch((e) => {
         reject(e)
         console.log(e)
-        console.log('Network Error')
-      })
-  })
-}
-
-/**
- * Pulls latest number of confirmed cases, deaths, and recovered by country
- * Query String Parameter: id
- * @param {ID} countryID
- * @returns {Promise} object with latest data by country
- */
-function requestDataByCountry(countryID) {
-  return new Promise((resolve, reject) => {
-    fetch(SERVER_2 + 'v2/locations/' + countryID)
-      .then(async (res) => {
-        let json = await res.json()
-        console.log(json)
-        resolve(json)
-      })
-      .catch((e) => {
-        reject(e)
-        console.error(e)
         console.log('Network Error')
       })
   })
@@ -59,6 +36,28 @@ function requestListOfCountries() {
           })
         })
         resolve(countries)
+      })
+      .catch((e) => {
+        reject(e)
+        console.error(e)
+        console.log('Network Error')
+      })
+  })
+}
+
+/**
+ * Pulls latest number of confirmed cases, deaths, and recovered by country
+ * API Query Parameter: slug
+ * @param {ID} slug
+ * @returns {Promise} object with latest data of the country
+ */
+function requestDataByCountry(slug) {
+  return new Promise((resolve, reject) => {
+    fetch(SERVER + 'total/country/' + slug)
+      .then(async (res) => {
+        let json = await res.json()
+        console.log(json)
+        resolve(json)
       })
       .catch((e) => {
         reject(e)
