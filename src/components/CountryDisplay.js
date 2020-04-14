e76b131e089ec3ebba820aaaac8c033e7e3e12ab
abir <abirchandrabhushan@gmail.com>
2020-04-14 16:57:36 +0530

    add table for country data

--- src/components/CountryDisplay.js
+++ src/components/CountryDisplay.js
@@ -1,47 +1,76 @@
-import React, { Component } from "react";
-import { AutoComplete, Badge, Row, Col } from "antd";
-import LoadingCard from "./LoadingCard";
+import React, { Component } from 'react'
+import { AutoComplete, Badge, Table } from 'antd'
+import LoadingCard from './LoadingCard'
+import { requestDataByCountry } from '../api/data'
+
+const schema = [
+  {
+    title: 'Country',
+    dataIndex: 'country',
+    key: 'country',
+  },
+  {
+    title: 'Change',
+    dataIndex: 'delta',
+    key: 'delta',
+    render: (delta) => {
+      return (
+        <Badge
+          count={delta}
+          className='site-badge-count-4'
+          style={{ backgroundColor: '#52c41a' }}
+        />
+      )
+    },
+  },
+  {
+    title: 'Confirmed',
+    dataIndex: 'confirmed',
+    key: 'confirmed',
+  },
+  {
+    title: 'Deaths',
+    dataIndex: 'deaths',
+    key: 'deaths',
+  },
+  {
+    title: 'Recovered',
+    dataIndex: 'recovered',
+    key: 'recovered',
+  },
+]
+
+const data = [
+
+]
 
 class CountryDisplay extends Component {
   render() {
-    const { options } = this.props;
+    const { options } = this.props
     if (!options) {
-      return <LoadingCard></LoadingCard>;
+      return <LoadingCard></LoadingCard>
     }
     return (
       <>
         <AutoComplete
           style={{
-            width: "100%",
-            marginTop: "5%",
+            width: '100%',
+            marginTop: '5%',
           }}
           options={options}
-          placeholder="Add Country"
+          placeholder='Add Country'
           filterOption={(inputValue, option) =>
             option.value.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1
           }
           onSelect={(value, option) => {
-            console.log(option);
+            console.log(option)
           }}
           allowClear={true}
         />
-        <Row style={{ marginTop: "5%" }}>
-          <Col span={8}>
-          <Badge
-              className="site-badge-count-109"
-              count={109}
-              style={{ backgroundColor: "#52c41a" }}
-            /><br/>
-            United States
-          </Col>
-          <Col span={16}>
-            <br/>
-            SS
-          </Col>
-        </Row>
+        <Table columns={schema} dataSource={data} style={{width: '100%', marginTop:'5%'}} />
       </>
-    );
+    )
   }
 }
 
-export default CountryDisplay;
+export default CountryDisplay
