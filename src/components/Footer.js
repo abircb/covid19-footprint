5564d7334b134bf1d22654325f7be5933fda7486
abir <abirchandrabhushan@gmail.com>
2020-06-25 01:09:18 +0530

    revert back to orignal footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,32 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'antd'
-import { ApiFilled, GithubFilled, RedoOutlined } from '@ant-design/icons'
+import { ApiFilled, GithubFilled } from '@ant-design/icons'
 
 class Footer extends Component {
   render() {
     return (
-      <>
-        <Row gutter={2} justify={'center'} style={{ textAlign: 'center' }}>
-          <Col span={2}>
-            <RedoOutlined onClick={() => console.log('nouveau')} />
-          </Col>
-        </Row>
-        <Row gutter={2} justify={'center'} style={{ textAlign: 'center' }}>
-          <Col span={2}>
-            <a
-              href={
-                'https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest'
-              }
-              target={'_blank'}
-              rel={'noopener noreferrer'}
-            >
-              <ApiFilled />
-            </a>
-          </Col>
-          <Col span={2}>
-            <a
-              href={'https://github.com/abircb/covid19-footprint'}
-              target={'_blank'}
-              rel={'noopener noreferrer'}
-            >
-              <GithubFilled />
-            </a>
-          </Col>
-        </Row>
-      </>
+      <Row gutter={2} justify={'center'} style={{ textAlign: 'center' }}>
+        <Col span={2}>
+          <a
+            href={
+              'https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest'
+            }
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+          >
+            <ApiFilled />
+          </a>
+        </Col>
+        <Col span={2}>
+          <a
+            href={'https://github.com/abircb/covid19-footprint'}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+          >
+            <GithubFilled />
+          </a>
+        </Col>
+      </Row>
     )
   }
 }
